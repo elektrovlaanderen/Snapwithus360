65a1aca340b29ebdc8477873fc2a8ce2d41d2b90
elektrovlaanderen <tom@elektrovlaanderen.be>
2026-09-23 16:46:32 +0200

    Add files via upload

---
+++ lichtplan/sw.js
@@ -0,0 +1,11 @@
+const C='lichtplan-v2';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()))});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener('fetch',e=>{
+  if(e.request.method!=='GET')return;
+  e.respondWith(caches.match(e.request,{ignoreSearch:true}).then(hit=>{
+    const net=fetch(e.request).then(r=>{if(r&&(r.ok||r.type==='opaque')){const cp=r.clone();caches.open(C).then(c=>c.put(e.request,cp))}return r}).catch(()=>hit);
+    return hit||net;
+  }));
+});
